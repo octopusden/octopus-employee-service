6202a5083766fcaeb33b5fc03fcf1ae41a338132
ALEXEY ZVEREV <ozonophore@gmail.com>
2023-01-10 13:55:50 +0300

    OCTOPUS-9 Add poms (#8)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,6 +11,27 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            pom {
+                name.set(project.name)
+                description.set("Octopus module: ${project.name}")
+                url.set("https://github.com/octopusden/octopus-employee-service.git")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/kzaporozhtsev/octopus-employee-service.git")
+                    connection.set("scm:git://github.com/octopusden/octopus-employee-service.git")
+                }
+                developers {
+                    developer {
+                        id.set("octopus")
+                        name.set("octopus")
+                    }
+                }
+            }
         }
     }
 }
