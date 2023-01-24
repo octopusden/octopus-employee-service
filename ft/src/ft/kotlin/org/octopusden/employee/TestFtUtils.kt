7babbeeaef26324aea5796be8f2c7d3504cb07c9
Alexey Zverev <ozonophore@gmail.com>
2023-01-24 22:00:10 +0400

    Rename env var

--- ft/src/ft/kotlin/org/octopusden/employee/TestFtUtils.kt
+++ ft/src/ft/kotlin/org/octopusden/employee/TestFtUtils.kt
@@ -4,7 +4,7 @@ import org.octopusden.employee.client.EmployeeServiceClient
 import org.octopusden.employee.client.impl.ClassicEmployeeServiceClient
 import org.octopusden.employee.client.impl.EmployeeServiceClientParametersProvider
 
-private const val TECHNICAL_USER_BEARER = "TECHNICAL_USER_BEARER"
+private const val TECHNICAL_USER_TOKEN = "TECHNICAL_USER_TOKEN"
 private const val FAKE_BEARER = "fake.bearer"
 private const val API_URL = "http://localhost:8080"
 private const val RETRY_IN_MILLIS = 180000
@@ -14,8 +14,8 @@ class TestFtUtils private constructor() {
         @JvmStatic
         fun getSecuredClient(): EmployeeServiceClient =
             ClassicEmployeeServiceClient(object : EmployeeServiceClientParametersProvider {
-                override fun getBearerToken(): String = System.getenv(TECHNICAL_USER_BEARER)
-                    ?: throw IllegalArgumentException("TECHNICAL_USER_BEARER(environment variable) must be not null")
+                override fun getBearerToken(): String = System.getenv(TECHNICAL_USER_TOKEN)
+                    ?: throw IllegalArgumentException("TECHNICAL_USER_TOKEN(environment variable) must be not null")
                 override fun getApiUrl() = API_URL
                 override fun getTimeRetryInMillis() = RETRY_IN_MILLIS
                 override fun getBasicCredentials(): String? = null
