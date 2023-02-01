6f08f4c777fed6974311f63ef8aa77c866eb5d42
ALEXEY ZVEREV <ozonophore@gmail.com>
2023-02-01 14:11:03 +0200

    JREP-205 Add the health check for oneC (#20)

---
+++ server/src/main/kotlin/org/octopusden/employee/actuator/OneCHealthIndicator.kt
@@ -0,0 +1,24 @@
+package org.octopusden.employee.actuator
+
+import org.octopusden.employee.service.onec.client.OneCClient
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+
+/**
+ * The component to check integration wit oneC
+ */
+@Component
+class OneCHealthIndicator(
+    private val oneCClient: OneCClient
+): HealthIndicator {
+
+    override fun health(): Health {
+        return try {
+            oneCClient.getHealth()
+            return Health.up().build()
+        } catch (e: Exception) {
+            return Health.down(e).build()
+        }
+    }
+}
