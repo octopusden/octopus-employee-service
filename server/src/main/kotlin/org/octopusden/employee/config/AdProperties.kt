31364485a1cc4096a6bc69ce8f4090637dd8569f
fishinitself <mikhail.ershov@gmail.com>
2026-07-07 15:26:13 +0300

    Fix AdProperties binding failure when ad.* properties are absent
    Spring Boot scans all @ConfigurationProperties classes regardless of conditional
    beans. AdProperties non-null constructor parameters caused an NPE during binding
    in deployments where no ad.url is configured. Add empty-string defaults so
    binding always succeeds; @ConditionalOnProperty("ad.url") still prevents AdConfig
    from being created when the property is absent.

--- server/src/main/kotlin/org/octopusden/employee/config/AdProperties.kt
+++ server/src/main/kotlin/org/octopusden/employee/config/AdProperties.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("ad")
 data class AdProperties(
-    val url: String,
-    val userDn: String,
-    val password: String,
-    val baseDn: String,
+    val url: String = "",
+    val userDn: String = "",
+    val password: String = "",
+    val baseDn: String = "",
 )
